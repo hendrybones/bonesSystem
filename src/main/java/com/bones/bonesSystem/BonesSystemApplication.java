95304e33061689d1d06e05ee0e0bad002afa1767
bones <hendrymwamburi56@gmail.com>
2021-10-22 13:52:45 +0300

    Initial commit

---
+++ src/main/java/com/bones/bonesSystem/BonesSystemApplication.java
@@ -0,0 +1,15 @@
+package com.bones.bonesSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+public class BonesSystemApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(BonesSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bones/bonesSystem/BonesSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bones.bonesSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BonesSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
